1263f7c48d9ddb97b5d72583b89313d968f689d3
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-03 13:40:20 +0700

    34. Filtering State

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -18,6 +18,12 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField} = this.state;
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
         <input
@@ -29,7 +35,7 @@ class App extends Component {
             );
           }}
         ></input>
-        <CardList monsters={this.state.monsters}></CardList>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
