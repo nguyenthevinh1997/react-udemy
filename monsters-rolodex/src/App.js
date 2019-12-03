c946aeecc3c004f5125d4b1b94456953abbc9d04
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-03 14:22:47 +0700

    36. Searchbox component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 // import logo from "./logo.svg";
 import "./App.css";
 
@@ -18,23 +20,22 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
-    const { monsters, searchField} = this.state;
+    const { monsters, searchField } = this.state;
     // const monsters = this.state.monsters;
     // const searchField = this.state.searchField;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      )
+    );
     return (
       <div className="App">
-        <input
-          type="password"
+        <SearchBox
           placeholder="search monsters"
-          onChange={e => {
+          handleChange={e => {
             this.setState({ searchField: e.target.value }, () =>
               console.log(this.state)
             );
           }}
-        ></input>
+        ></SearchBox>
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
