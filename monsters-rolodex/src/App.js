cd2f285771d093bc906f242f5c65e761aaec1763
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-03 10:28:00 +0700

    Card List component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,33 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import { CardList } from "./components/card-list/card-list.component";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      string: 'Hello Yihua Zang'
+      monsters: []
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button>Change Text</button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList monsters={this.state.monsters} >
+        </CardList>
       </div>
     );
   }
---
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+export const Card = (props) => (
+    <div>
+<h1>{ props.monster.name }</h1>
+</div>
+)
