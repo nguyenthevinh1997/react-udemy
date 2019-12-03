9a93406438ab08cddaa3c1d4d95ffe6bcfd86ed8
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-03 15:09:16 +0700

    Revert "43. Deploy git"
    This reverts commit 2a32d16a96a0657e0587d0c5a43ab9e938da5e79.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
     );
     return (
       <div className="App">
-      <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
