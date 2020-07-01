93c1f996dd861bf20f659543b1b5b5ef8461bb33
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-01 11:09:30 +0700

    45. Deploying Our App

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange = (e)=>{
-    this.setState({searchField:e.target.value})
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
   }
   render() {
     const { monsters, searchField } = this.state;
@@ -32,6 +32,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
