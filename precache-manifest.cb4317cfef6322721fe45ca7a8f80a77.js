6d20ee2ddf2600a6fab9529d6d48198a06fa58c8
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-01 10:55:07 +0700

    Updates

---
+++ precache-manifest.cb4317cfef6322721fe45ca7a8f80a77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b743d6e9473d8bf257ecc39ba31ba74a",
+    "url": "/react-udemy/index.html"
+  },
+  {
+    "revision": "db4cbfe561cf2dc49abd",
+    "url": "/react-udemy/static/css/main.3cf81b4f.chunk.css"
+  },
+  {
+    "revision": "6246d942ed27f5c53c53",
+    "url": "/react-udemy/static/js/2.035e7d4a.chunk.js"
+  },
+  {
+    "revision": "db4cbfe561cf2dc49abd",
+    "url": "/react-udemy/static/js/main.aeedd3b9.chunk.js"
+  },
+  {
+    "revision": "2e6a81dd853ed5ee0d5d",
+    "url": "/react-udemy/static/js/runtime-main.3f2848b8.js"
+  }
+]);
